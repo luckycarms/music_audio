e6c43d8b9d48021f30b8a13faaed647ffe33dd68
luckycarms <caleha23@gmail.com>
2024-12-19 21:38:12 -0500

    updated practice project

---
+++ Dashboard.py
@@ -0,0 +1,11 @@
+#Creating a Dashboard
+from dash import Dash, html, Input, Output
+import plotly.express as px
+
+import dash_bootstrap_components as dbc
+import pandas as pd
+
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+
