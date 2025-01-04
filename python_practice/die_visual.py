40e942b75ea5161f166f49a3cdb172f071fdf5bd
luckycarms <caleha23@gmail.com>
2025-01-03 22:24:05 -0500

    update to python_practice

---
+++ python_practice/die_visual.py
@@ -0,0 +1,32 @@
+import plotly.express as px
+
+from die import Die
+
+
+# Create a D6
+die = Die()
+
+# Make some rolls, and store results in a list.
+results =[]
+
+for roll_num in range(1000):
+    result = die.roll()
+    results.append(result)
+
+#print(results)
+
+# Anualyze the results.
+frequencies = []
+poss_results = range(1,die.num_sides + 1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+#print(frequencies)
+
+# Visualize the results
+title = "Results of Rolling one D6 1,000 Times"
+labels = {'x': 'Result', 'y': 'Frequency of Result'}
+
+fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+fig.show()
