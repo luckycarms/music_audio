9c6c1099d071843bb90a3c29ea838784f9053b69
luckycarms <caleha23@gmail.com>
2025-01-15 08:08:19 -0500

    updated python file

--- movies_practice/movies.py
+++ movies_practice/movies.py
@@ -8,7 +8,7 @@ import matplotlib
 plt.style.use('ggplot')
 from matplotlib.pyplot import figure
 
-%matplotlib inline
+
 matplotlib.rcParams['figure.figsize'] = (12,8)
 
 pd.options.mode.chained_assignment = None
